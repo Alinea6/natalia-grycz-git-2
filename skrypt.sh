7cca9e7f9869a62feea1f12b5d71c4ea8e359b59
Student DSW <studentdsw@Student.DSW>
2022-07-09 10:24:28 +0200

    added logs flag functionality

--- skrypt.sh
+++ skrypt.sh
@@ -6,5 +6,14 @@ while test $# -gt 0; do
 			echo "Current date: $now"
 			exit 1
 			;;
+		--logs)
+			for i in {1..100}
+			do
+				filename="log${i}.txt"
+				touch $filename
+				echo "${filename}, skrypt.sh, $(date)" >> $filename
+			done
+			exit 1
+			;;
 	esac
 done
